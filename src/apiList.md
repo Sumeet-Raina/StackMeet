cc54b4bcb1b4b8ad23daf23f26e18e032c2862e4
Sumeet Raina <sumeet@Mac.broadband>
2025-05-17 22:18:10 +0100

    created aceept/reject request endpoint

--- src/apiList.md
+++ src/apiList.md
@@ -14,7 +14,8 @@ profileRouter
 
 connectionRequestRouter
 
-- POST /request/send/interested:status/:userId
+- POST /request/send/interested/:status/:userId
+- POST /request/send/ignored/:status/:userId
 
 - POST /request/review/accepted/:requestId
 - POST /request/review/rejected/:requestId
--- src/routes/request.js
+++ src/routes/request.js
@@ -56,4 +56,39 @@ requestRouter.post(
   }
 );
 
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const status = req.params.status;
+      const requestId = req.params.requestId;
+      const allowedStatus = ["accepted", "rejected"];
+
+      if (!allowedStatus.includes(status)) {
+        res.status(400).json({ message: "Invalid status type " + status });
+      }
+
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "Connection request not found" });
+      }
+      connectionRequest.status = status;
+      const data = await connectionRequest.save();
+      res.json({
+        message: "Connection request " + status,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("Error: " + err.message);
+    }
+  }
+);
 module.exports = requestRouter;
